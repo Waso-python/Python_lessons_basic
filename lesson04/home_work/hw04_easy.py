3f4dddf47f1d07a7a529a0a1472bdd381eaace86
Waso-python <waso.82@gmail.com>
2018-09-17 15:58:08 +0300

    for check

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -1,14 +1,45 @@
 # Все задачи текущего блока решите с помощью генераторов списков!
-
+import random
 # Задание-1:
 # Дан список, заполненный произвольными целыми числами. 
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+
+def rand_int(count, fr, to):
+    lst_g = [random.randint(fr, to) for _ in range(count)]
+    return lst_g
+
+
+def stepen(lst):
+    new_lst = [el*el for el in lst]
+    return new_lst
+
+
+a = rand_int(8,-20,20)
+b = stepen(a)
+print(a)
+print(b)
+
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+a = ["apple", "grapefruit", "strawberry", "orange"]
+b = ["Banana", "Guava", "Kiwi", "orange", "apple"]
+
+def compare_lst(lst1,lst2):
+    lst3 = list()
+    for el in lst1:
+        if el in lst2:
+            lst3.append(el)
+        else: pass
+    return lst3
+print(compare_lst(a,b))
+
+
+
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +47,18 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+a1 = list()
+a2 = list()
+a3 = list()
+
+lst = rand_int(10, -15, 15)
+
+a1 = [el for el in lst if not el % 3]
+a2 = [el for el in lst if el >= 0]
+a3 = [el for el in lst if el % 4]
+
+print(lst)
+print(a1)
+print(a2)
+print(a3)
