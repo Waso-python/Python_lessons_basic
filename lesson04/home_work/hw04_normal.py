ba4795a59f4f5d952a652c56711dacbae517a122
Waso-python <waso.82@gmail.com>
2018-09-19 21:39:19 +0300

    for check regexp

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,3 +1,6 @@
+import re
+import os
+import random
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
@@ -20,6 +23,8 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+result = re.split(r'[A-Z]+', line)
+print(result)
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -45,9 +50,29 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
-# Задание-3:
+pattern = r'[a-z]{2}([A-Z]+)[A-Z]{2}'
+#for el in re.finditer(pattern,line_2):
+#    print(el.group(1))
+print([el.group(1) for el in re.finditer(pattern,line_2)])
+
+
+
+       # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+string1 = ""
+for _ in range(2500):
+       string1 = string1 + str(random.randint(0,9))
+path = os.path.join('files', 'text.txt')
+f = open(path, 'w', encoding='UTF-8')
+f.write(string1)
+print(string1)
+f = open(path, 'r', encoding='UTF-8')
+string2 = f.read()
+pattern = r'(.)\1+'
+print([el.group(0) for el in re.finditer(pattern,string2)])
+
+f.close()
